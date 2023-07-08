6af8df9cf3e7bb586151b8e0e509bcdf55ea413d
Felipe <felipe.vilela01@outlook.com>
2023-07-08 01:57:55 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto criado a partir da trilha discovery da plataforma Rocketseat
---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+  const html = document.documentElement
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("ligth")) {
+    html.classList.remove("ligth")
+  } else {
+    html.classList.add("ligth")
+  }
+
+  if (html.classList.contains("ligth")) {
+   img.setAttribute('src', './assets/avatar-ligth.png')
+   img.setAttribute("alt", "Foto Felipe com sorriso")
+  } else {
+   img.setAttribute("src", "./assets/avatar-dark.png")
+   img.setAttribute("alt", "Foto Felipe sem sorriso")
+  }
+}
