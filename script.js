6df95ae612c0d57486c9e1fd17f87f100894ef84
Felipe <felipe.vilela01@outlook.com>
2023-09-23 21:30:48 -0300

    atualizacao

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function toggleMode() {
   const html = document.documentElement
   const img = document.querySelector("#profile img")
 
-  if (html.classList.contains("ligth")) {
-    html.classList.remove("ligth")
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
   } else {
-    html.classList.add("ligth")
+    html.classList.add("light")
   }
 
-  if (html.classList.contains("ligth")) {
-   img.setAttribute('src', './assets/avatar-ligth.png')
-   img.setAttribute("alt", "Foto Felipe com sorriso")
+  if (html.classList.contains("light")) {
+   img.setAttribute('src', './assets/avatar-light.png')
+   img.setAttribute("alt", "Foto Felipe Vilela")
   } else {
    img.setAttribute("src", "./assets/avatar-dark.png")
-   img.setAttribute("alt", "Foto Felipe sem sorriso")
+   img.setAttribute("alt", "Foto Felipe Vilela")
   }
 
 }
